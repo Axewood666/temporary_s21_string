24acad3df3a603f53fdf6a97ecb7ff038cae22a7
Axewood <igor.zhugar@mail.ru>
2024-11-30 01:15:19 +0300

    init commit

---
+++ s21_string.h
@@ -0,0 +1,9 @@
+#define s21_size_t unsigned long //maybe ll xz
+#define s21_NULL (void*)0  // Null pointer constant. 
+// The macro shall expand to an integer constant expression with the value 0 cast to type void*
+
+s21_size_t s21_strlen(const char* str);
+char *s21_strchr(const char *str, int c);
+char *s21_strrchr(const char *str, int c);
+char *s21_strpbrk(const char *str1, const char *str2);
+void *s21_memchr(const void *str, int c, s21_size_t n);
