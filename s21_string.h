726cb045f9d51a16d40e246ff9bc5f8e4c56a353
Axewood <igor.zhugar@mail.ru>
2024-12-01 14:19:15 +0300

    clang format

--- s21_string.h
+++ s21_string.h
@@ -1,8 +1,9 @@
-#define s21_size_t unsigned long //maybe ll xz
-#define s21_NULL (void*)0  // Null pointer constant. 
-// The macro shall expand to an integer constant expression with the value 0 cast to type void*
+#define s21_size_t unsigned long // maybe ll xz
+#define s21_NULL (void *)0       // Null pointer constant.
+// The macro shall expand to an integer constant expression with the value 0
+// cast to type void*
 
-s21_size_t s21_strlen(const char* str);
+s21_size_t s21_strlen(const char *str);
 char *s21_strchr(const char *str, int c);
 char *s21_strrchr(const char *str, int c);
 char *s21_strpbrk(const char *str1, const char *str2);
