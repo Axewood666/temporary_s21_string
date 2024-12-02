00feb3676205e58c474638a25b113c38596e13ef
Axewood <igor.zhugar@mail.ru>
2024-12-02 18:13:36 +0300

    add tests.c in report

--- s21_string.c
+++ s21_string.c
@@ -103,8 +103,6 @@ void *s21_memcpy(void *dest, const void *src, s21_size_t n) {
 }
 
 void *s21_memset(void *str, int c, s21_size_t n) {
-  if (str == s21_NULL) return s21_NULL;
-
   char *char_ptr = (char *)str;
   for (s21_size_t i = 0; i < n; i++) {
     char_ptr[i] = (char)c;
