25908c0fa606b6bd4a82d51ac391f9690aef09fb
Austin Mars <austinma@si-n6.kzn.21-school.ru>
2024-12-11 19:05:32 +0300

    trim done

--- src/s21_csharp_string.c
+++ src/s21_csharp_string.c
@@ -23,3 +23,35 @@ void *insert(const char *src, const char *str, size_t start_index) {
     return returnStr; 
 }
 
+void *trim(const char *src, const char *trim_chars){
+    char *returnStr;
+    if(src!=s21_NULL && trim_chars!=s21_NULL){
+        int lenSrc = s21_strlen(src);
+        int start = 0, end = lenSrc;
+        while(start < lenSrc && s21_strchr(trim_chars,src[start]) != NULL){
+            start++;
+        }
+        while(end>0 && s21_strchr(trim_chars,src[end])!=NULL){
+            end--;
+        }
+        if(start>=end){
+            returnStr = malloc(sizeof(char));
+            if(returnStr == s21_NULL){
+                returnStr = s21_NULL;
+            }
+            returnStr[0] = '\0';
+        }else{
+            int retLen = end - start + 1;
+            returnStr = malloc(sizeof(char)*retLen);
+            if(returnStr == s21_NULL){
+                returnStr = s21_NULL;
+            }else{
+                s21_strncpy(returnStr,src+start,retLen-1);
+                returnStr[retLen-1]='\0';
+            }
+        }
+    }else{
+        returnStr = s21_NULL;
+    }
+    return returnStr;
+}
