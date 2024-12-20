45322b4d6cf5e4530def865a7a61ac3dd41599d2
Drake Chante <drakecha@et-f1.kzn.21-school.ru>
2024-12-20 20:54:44 +0300

    develop: add strerror. update makefile

--- src/s21_string.h
+++ src/s21_string.h
@@ -1,8 +1,8 @@
 #ifndef S21_STRING_H
 #define S21_STRING_H
 
-#define s21_size_t unsigned long // maybe ll xz
-#define s21_NULL (void *)0       // Null pointer constant.
+#define s21_size_t unsigned long  // maybe ll xz
+#define s21_NULL (void *)0        // Null pointer constant.
 // The macro shall expand to an integer constant expression with the value 0
 // cast to type void*
 
@@ -20,4 +20,6 @@ s21_size_t s21_strcspn(const char *str1, const char *str2);
 char *s21_strtok(char *str, const char *delim);
 char *s21_strncat(char *dest, const char *src, s21_size_t n);
 char *s21_strstr(const char *haystack, const char *needle);
+char *s21_strerror(int errnum);
+
 #endif
