e1dc87fec9643bf1b5d67a990685306c7c26f1fd
Tytos Boobarella <tytosboo@et-f3.kzn.21-school.ru>
2024-12-20 21:08:57 +0300

    add d flag

--- src/s21_sprintf/s21_sprintf.c
+++ src/s21_sprintf/s21_sprintf.c
@@ -1,17 +1,78 @@
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
 
-int s21_sprintf(char *str, const char *format, ...);
+#include "../s21_string.h"
 
-int main()
-{
-    char buffer[50];
+int s21_sprintf(char *str, char *format, ...);
+void intToStr(int num, char *str);
+int main() {
+  char buffer1[50];
+  char buffer2[50];
+  int number = 0;
+  scanf("%d", &number);
 
-    int result = sprintf(buffer, "Hello, %ld world!", -9223372036854775807);
-    printf("Result: %d\n", result);
-    printf("Buffer: %s\n", buffer);
-    printf("Short: %d", (short int)32768);
+  s21_sprintf(buffer1, "%d", number);
+  sprintf(buffer2, "%d", number);
 
-    return 0;
+  printf("%s\n%s", buffer1, buffer2);
+  return 0;
+}
+int s21_sprintf(char *str, char *format, ...) {
+  int d;
+  double f;
+  va_list factor;            // указатель на необязательный параметр
+  va_start(factor, format);  // устанавливаем указатель
+
+  for (char *c = format; *c; c++) {
+    if (*c != '%') {
+      printf("%c", *c);
+    } else {
+      switch (*++c)  // если символ - %, то переходим к следующему символу
+      {
+        case 'd':
+          d = va_arg(factor, int);
+          char buffer[12];
+          intToStr(d, buffer);
+          s21_strncat(str, buffer, 11);
+          break;
+        case 'f':
+          f = va_arg(factor, double);
+          printf("%.2lf", f);
+          break;
+        default:
+          printf("%c", *c);
+      }
+    }
+  }
+  va_end(factor);
+  return 0;
+}
+
+void intToStr(int num, char *str) {
+  int i = 0;
+  int sign = num;
+
+  if (num == 0) {
+    str[i++] = '0';
+  }
+
+  if (num < 0) num = -num;
+
+  while (num > 0) {
+    str[i++] = num % 10 + '0';
+    num /= 10;
+  }
+
+  if (sign < 0) {
+    str[i++] = '-';
+  }
+
+  str[i] = '\0';
+
+  for (int j = 0, k = i - 1; j < k; j++, k--) {
+    char temp = str[j];
+    str[j] = str[k];
+    str[k] = temp;
+  }
 }
