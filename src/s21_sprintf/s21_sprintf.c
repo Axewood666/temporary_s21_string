5d30a69de0d6f4e6cdfbe61df003995d8314dd12
Austin Mars <austinma@si-j4.kzn.21-school.ru>
2024-12-25 16:37:40 +0300

    fixed format

--- src/s21_sprintf/s21_sprintf.c
+++ src/s21_sprintf/s21_sprintf.c
@@ -4,7 +4,6 @@
 
 #include "../s21_string.h"
 
-
 // Чек-лист
 // Сделать парсер ✅
 // Спецификаторы
@@ -12,11 +11,7 @@
 // Ширина
 // Точность
 // Длина
-// 
-
-
-
-
+//
 
 int s21_sprintf(char *str, const char *format, ...);
 void int_to_string(int num, char *str);
@@ -24,136 +19,129 @@ int is_number(const char *input_char);
 int is_digit(const char input_char);
 
 typedef struct {
-    char flag[3];    
-    int width;         
-    int precision;      
-    char length;        
-    char specifier;     
+  char flag[3];
+  int width;
+  int precision;
+  char length;
+  char specifier;
 } FormatSpec;
 
-
-
-int parse_znach(int index,char *format_str,FormatSpec *infoSpec){
-  //парсинг флагов
-  for(++index;s21_strchr("-+ ",format_str[index]);index++){
-    (infoSpec->flag)[3-s21_strlen(s21_strchr("-+ ",format_str[index]))] = 'y';
+int parse_znach(int index, char *format_str, FormatSpec *infoSpec) {
+  // парсинг флагов
+  for (++index; s21_strchr("-+ ", format_str[index]); index++) {
+    (infoSpec->flag)[3 - s21_strlen(s21_strchr("-+ ", format_str[index]))] =
+        'y';
   }
-  //парсинг длины
-  for(;s21_strchr("0123456789",format_str[index]);index++){
+  // парсинг длины
+  for (; s21_strchr("0123456789", format_str[index]); index++) {
     infoSpec->width = (infoSpec->width * 10) + (format_str[index] - '0');
   }
-  //парсинг длины после запятой
-  if(format_str[index] == '.'){
-    for(++index;s21_strchr("0123456789",format_str[index]);index++){
-      infoSpec->precision = (infoSpec->precision * 10) + (format_str[index] - '0');
+  // парсинг длины после запятой
+  if (format_str[index] == '.') {
+    for (++index; s21_strchr("0123456789", format_str[index]); index++) {
+      infoSpec->precision =
+          (infoSpec->precision * 10) + (format_str[index] - '0');
     }
   }
-  //парсинг флагов h и l
-  for(;s21_strchr("hl",format_str[index]);index++){
+  // парсинг флагов h и l
+  for (; s21_strchr("hl", format_str[index]); index++) {
     infoSpec->length = format_str[index];
   }
-  //парсинг спецификаторов c, d, f, s, u
-  for(;s21_strchr("cdfsu",format_str[index]);index++){
+  // парсинг спецификаторов c, d, f, s, u
+  for (; s21_strchr("cdfsu", format_str[index]); index++) {
     infoSpec->specifier = format_str[index];
   }
   return index;
 }
 
-
-
-
-
-void handle_d_specifier(char *buffer, int *buffer_index, FormatSpec *spec, va_list *args){
+void handle_d_specifier(char *buffer, int *buffer_index, FormatSpec *spec,
+                        va_list *args) {
   int number;
   char number_str[21];
 
   // получаем число
-  if(spec->length == 'h'){
-    number = (short)va_arg(*args,int);
-  }else if(spec->length == 'l'){
-    number  = va_arg(*args,long);
-  }else{
-    number  = va_arg(*args,int);
+  if (spec->length == 'h') {
+    number = (short)va_arg(*args, int);
+  } else if (spec->length == 'l') {
+    number = va_arg(*args, long);
+  } else {
+    number = va_arg(*args, int);
   }
-  
+
   // Преобразуем число в строку
-  int_to_string(number,&number_str[1]);
+  int_to_string(number, &number_str[1]);
   // Точность
   int len = s21_strlen(number_str);
-  if(spec->precision > 0){
-    
-    if(len < spec->precision){
+  if (spec->precision > 0) {
+
+    if (len < spec->precision) {
       int zeros = spec->precision - len;
-      for(int i = len;i>=0;i--){
-        number_str[i+zeros] = number_str[i];
+      for (int i = len; i >= 0; i--) {
+        number_str[i + zeros] = number_str[i];
       }
     }
   }
 
   // знак
-  if(number < 0){
+  if (number < 0) {
     number_str[0] = '-';
-  }else if(spec->flag[1] == 'y'){
+  } else if (spec->flag[1] == 'y') {
     number_str[0] = '+';
-  }else if(spec->flag[2] == 'y'){
+  } else if (spec->flag[2] == 'y') {
     number_str[0] = ' ';
   }
 
   int padding = spec->width - len;
-  if(padding > 0){
-    if(spec->flag[0]=='y'){
+  if (padding > 0) {
+    if (spec->flag[0] == 'y') {
 
-      for(int i = 0;i<len;i++){
+      for (int i = 0; i < len; i++) {
         buffer[(*buffer_index)++] = number_str[i];
       }
-      for(int i = 0;i<padding;i++){
+      for (int i = 0; i < padding; i++) {
         buffer[(*buffer_index)++] = ' ';
       }
 
-    } else{
+    } else {
       for (int i = 0; i < padding; i++) {
-          buffer[(*buffer_index)++] = ' ';
+        buffer[(*buffer_index)++] = ' ';
       }
       for (int i = 0; i < len; i++) {
-          buffer[(*buffer_index)++] = number_str[i];
+        buffer[(*buffer_index)++] = number_str[i];
       }
     }
-  } else{
+  } else {
     for (int i = 0; i < len; i++) {
-          buffer[(*buffer_index)++] = number_str[i];
-      }
+      buffer[(*buffer_index)++] = number_str[i];
+    }
   }
-
 }
 
-int temp_sprintf(char *str, const char *format,...){
-  
+int temp_sprintf(char *str, const char *format, ...) {
+
   va_list factor;
   va_start(factor, format);
   char *temp_format = (char *)calloc(s21_strlen(format), sizeof(char));
   s21_strncpy(temp_format, format, s21_strlen(format));
 
   int buffer_index = 0;
-  for(int i = 0;temp_format[i]!='\0';i++){
-    if(temp_format[i] == '%'){
-      FormatSpec info = {"nnn",0,-1,'n','n'};
-      i = parse_znach(i,temp_format,&info);
+  for (int i = 0; temp_format[i] != '\0'; i++) {
+    if (temp_format[i] == '%') {
+      FormatSpec info = {"nnn", 0, -1, 'n', 'n'};
+      i = parse_znach(i, temp_format, &info);
 
-      if(info.specifier == 'd'){
+      if (info.specifier == 'd') {
         printf("YA TUT");
         handle_d_specifier(str, &buffer_index, &info, &factor);
       }
-    }else {
-        str[buffer_index++] = format[i];  // Копирование обычных символов
+    } else {
+      str[buffer_index++] = format[i]; // Копирование обычных символов
     }
   }
-  str[buffer_index] = '\0';  // Завершение строки
+  str[buffer_index] = '\0'; // Завершение строки
   va_end(factor);
   return 1;
 }
-
-
-
 
 int main() {
   char buffer[100];
@@ -161,26 +149,6 @@ int main() {
   printf("%s\n", buffer);
   return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 int is_number(const char *input_str) {
   int result = 1;
@@ -206,7 +174,8 @@ void int_to_string(int num, char *str) {
     str[i++] = '0';
   }
 
-  if (num < 0) num = -num;
+  if (num < 0)
+    num = -num;
 
   while (num > 0) {
     str[i++] = num % 10 + '0';
