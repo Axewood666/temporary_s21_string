cb99c9f60a4961e48308e44760505b438d785554
Austin Mars <austinma@si-j4.kzn.21-school.ru>
2024-12-25 15:19:31 +0300

    added

--- src/s21_sprintf/s21_sprintf.c
+++ src/s21_sprintf/s21_sprintf.c
@@ -60,6 +60,74 @@ int parse_znach(int index,char *format_str,FormatSpec *infoSpec){
 }
 
 
+
+
+
+void handle_d_specifier(char *buffer, int *buffer_index, FormatSpec *spec, va_list *args){
+  int number;
+  char number_str[20];
+
+  // получаем число
+  if(spec->length == 'h'){
+    number = (short)va_arg(*args,int);
+  }else if(spec->length == 'l'){
+    number  = va_arg(*args,long);
+  }else{
+    number  = va_arg(*args,int);
+  }
+  // Преобразуем число в строку
+  int_to_string(number,number_str);
+  // Точность
+  int len = s21_strlen(number_str);
+  if(spec->precision > 0){
+    
+    if(len < spec->precision){
+      int zeros = spec->precision - len;
+      for(int i = len;i>=0;i--){
+        number_str[i+zeros] = number_str[i];
+      }
+      for(int i = 0;i<zeros;i++){
+        number_str[i] = '0';
+      }
+    }
+  }
+
+  // знак
+  if(number < 0){
+    buffer[*(buffer_index)++] = '-';
+  }else if(spec->flag[1] == 'y'){
+    buffer[*(buffer_index)++] = '+';
+  }else if(spec->flag[2] == 'y'){
+    buffer[*(buffer_index)++] = ' ';
+  }
+
+  int padding = spec->width - len;
+  if(padding > 0){
+    if(spec->flag[0]=='y'){
+
+      for(int i = 0;i<len;i++){
+        buffer[*(buffer_index)++] = number_str[i];
+      }
+      for(int i = 0;i<padding;i++){
+        buffer[*(buffer_index)++] = ' ';
+      }
+
+    } else{
+      for (int i = 0; i < padding; i++) {
+          buffer[(*buffer_index)++] = ' ';
+      }
+      for (int i = 0; i < len; i++) {
+          buffer[(*buffer_index)++] = number_str[i];
+      }
+    }
+  } else{
+    for (int i = 0; i < len; i++) {
+          buffer[(*buffer_index)++] = number_str[i];
+      }
+  }
+
+}
+
 int temp_sprintf(char *str, const char *format,...){
   
   va_list factor;
@@ -67,28 +135,47 @@ int temp_sprintf(char *str, const char *format,...){
   char *temp_format = (char *)calloc(s21_strlen(format), sizeof(char));
   s21_strncpy(temp_format, format, s21_strlen(format));
 
-
+  int buffer_index = 0;
   for(int i = 0;temp_format[i]!='\0';i++){
     if(temp_format[i] == '%'){
-      FormatSpec info = {"nnn",0,0,'n','n'};
+      FormatSpec info = {"nnn",0,-1,'n','n'};
       i = parse_znach(i,temp_format,&info);
+      if(info.specifier == 'd'){
+        handle_d_specifier(str, &buffer_index, &info, &factor);
+      }
     }
   }
   return 1;
 }
 
+
+
+
 int main() {
-    const char Amirka[50] = "%10.3f вот такойasdasdasdas %10d";
-    char Amir[100];
-    
-
-    temp_sprintf(Amir, Amirka, 100.564,228);
-    
-
-    printf("%s\n", Amir); 
-    
-    return 0;
+  char buffer[50];
+  s21_sprintf(buffer, "Number: %+10d, Another: %-05d", 42, -123);
+  printf("%s\n", buffer);
+  return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 int is_number(const char *input_str) {
