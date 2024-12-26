98e75797d8cf7a3404c8ab042eaae952661de870
Austin Mars <austinma@si-j2.kzn.21-school.ru>
2024-12-26 18:39:34 +0300

    added %f

--- src/s21_sprintf/s21_sprintf.h
+++ src/s21_sprintf/s21_sprintf.h
@@ -20,6 +20,8 @@ void handle_c_specifier(char *buffer, int *buffer_index, FormatSpec *spec,
                         va_list *args);
 void handle_s_specifier(char *buffer, int *buffer_index, FormatSpec *spec,
                         va_list *args);
+void handle_f_specifier(char *buffer, int *buffer_index, FormatSpec *spec,
+                        va_list *args);
 void int_to_string_unsign(int num, char *str);
 void long_int_to_string_unsign(long int num, char *str);
 void short_int_to_string_unsign(short int num, char *str);
@@ -29,3 +31,4 @@ void flags_handling_int_specifiers(char *number_string, int number, int *length,
                                    char *flags);
 void width_handling_int_specifiers(char *number_string, int *length, int width,
                                    char flag);
+int float_to_string(double number, char*str,int precision);
