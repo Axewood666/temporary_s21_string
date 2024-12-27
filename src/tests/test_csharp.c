42ee7d58749ed1354b94e25f4ec052134e1ae7a3
Ngan Jenifer <nganjeni@si-k5.kzn.21-school.ru>
2024-12-27 18:49:41 +0300

    тесты чтобы амирчик проверил, потом еще добавлю

--- src/tests/test_csharp.c
+++ src/tests/test_csharp.c
@@ -15,11 +15,39 @@ START_TEST(test_to_upper) {
 }
 END_TEST
 
+START_TEST(test_to_lower) {
+  char *line1 = "123QWE123";
+  char *line2 = "123qwe123";
+  char *res = to_lower(line1);
+  ck_assert_str_eq(res, line2);
+  free(res);
+  char *line3 = NULL;
+  char *res2 = to_lower(line3);
+  ck_assert_ptr_null(res2);
+}
+END_TEST
+
+START_TEST(test_insert) {
+  char *line1 = "123QWE123";
+  char *line2 = "456";
+  char *actual = "123456QWE123";
+  char *res = insert(line1, line2, 3);
+  ck_assert_str_eq(res, actual);
+  free(res);
+
+  char *line3 = NULL;
+  char *res2 = insert(line3, line2, 1);
+  ck_assert_ptr_null(res2);
+}
+END_TEST
+
 Suite *test_csharp(void) {
   Suite *s = suite_create("Csharp test");
   TCase *tc = tcase_create("Tests");
 
   tcase_add_test(tc, test_to_upper);
+  tcase_add_test(tc, test_to_lower);
+  tcase_add_test(tc, test_insert);
   suite_add_tcase(s, tc);
   return s;
 }
