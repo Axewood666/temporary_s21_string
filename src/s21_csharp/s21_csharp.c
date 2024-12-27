adf4faf029a6c7fd30e1cab89cbd8794b980dec1
austinma <austinma@student.21-school.ru>
2024-12-27 17:38:52 +0300

    style csharp

--- src/s21_csharp/s21_csharp.c
+++ src/s21_csharp/s21_csharp.c
@@ -1,33 +1,33 @@
 #include "s21_csharp.h"
 
 void *insert(const char *src, const char *str, s21_size_t start_index) {
-  char *returnStr = s21_NULL;
+  char *return_str = s21_NULL;
   if (src != s21_NULL && str != s21_NULL) {
-    s21_size_t lenSrc = s21_strlen(src);
-    int lenStr = s21_strlen(str);
-    if (start_index > lenSrc) {
-      returnStr = s21_NULL;
+    s21_size_t len_src = s21_strlen(src);
+    int len_str = s21_strlen(str);
+    if (start_index > len_src) {
+      return_str = s21_NULL;
     } else {
-      int sumLen = lenSrc + lenStr + 1;
-      char *newStr = malloc(sumLen * sizeof(char));
-      if (newStr != s21_NULL) {
-        s21_strncpy(newStr, src, start_index);
-        s21_strncpy(newStr + start_index, str, lenStr);
-        s21_strncpy(newStr + start_index + lenStr, src + start_index,
-                    lenSrc - start_index);
-        newStr[sumLen - 1] = '\0';
-        returnStr = newStr;
+      int sum_len = len_src + len_str + 1;
+      char *new_str = malloc(sum_len * sizeof(char));
+      if (new_str != s21_NULL) {
+        s21_strncpy(new_str, src, start_index);
+        s21_strncpy(new_str + start_index, str, len_str);
+        s21_strncpy(new_str + start_index + len_str, src + start_index,
+                    len_src - start_index);
+        newStr[sum_len - 1] = '\0';
+        return_str = new_str;
       }
     }
   }
-  return returnStr;
+  return return_str;
 }
 
 void *trim(const char *src, const char *trim_chars) {
-  char *returnStr;
+  char *return_str;
   if (src != s21_NULL && trim_chars != s21_NULL) {
-    int lenSrc = s21_strlen(src);
-    int start = 0, end = lenSrc;
+    int len_src = s21_strlen(src);
+    int start = 0, end = len_src;
     while (start < lenSrc && s21_strchr(trim_chars, src[start]) != NULL) {
       start++;
     }
@@ -35,25 +35,25 @@ void *trim(const char *src, const char *trim_chars) {
       end--;
     }
     if (start >= end) {
-      returnStr = malloc(sizeof(char));
-      if (returnStr == s21_NULL) {
-        returnStr = s21_NULL;
+      return_str = malloc(sizeof(char));
+      if (return_str == s21_NULL) {
+        return_str = s21_NULL;
       }
-      returnStr[0] = '\0';
+      return_str[0] = '\0';
     } else {
-      int retLen = end - start + 1;
-      returnStr = malloc(sizeof(char) * retLen);
-      if (returnStr == s21_NULL) {
-        returnStr = s21_NULL;
+      int ret_len = end - start + 1;
+      return_str = malloc(sizeof(char) * ret_len);
+      if (return_str == s21_NULL) {
+        return_str = s21_NULL;
       } else {
-        s21_strncpy(returnStr, src + start, retLen - 1);
-        returnStr[retLen - 1] = '\0';
+        s21_strncpy(return_str, src + start, ret_len - 1);
+        return_str[ret_len - 1] = '\0';
       }
     }
   } else {
-    returnStr = s21_NULL;
+    return_str = s21_NULL;
   }
-  return returnStr;
+  return return_str;
 }
 
 void *to_upper(const char *str) {
